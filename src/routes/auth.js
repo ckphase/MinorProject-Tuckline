145b9c1f74565000cef918a321489d943289ae92
ckphase <worldchampion202428@gmail.com>
2025-04-12 00:07:11 +0530

    chore: improve readability

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,20 +9,28 @@ const users = [
 
 // GET
 router.get('/login', (req, res) => res.render('auth/login'));
+
 router.get('/register/:role', (req, res) => {
   const role = req.params.role;
-  if (!['admin', 'customer'].includes(role))
+  if (!['admin', 'customer'].includes(role)) {
     return res.status(404).send('Not found');
+  }
+
   res.render('auth/register', { role });
 });
 
 // POST
 router.post('/register', (req, res) => {
   const { username, password, role } = req.body;
-  if (!username || !password || !role)
+  if (!username || !password || !role) {
     return res.send('All fields are required.');
+  }
+
   const exists = users.find((u) => u.username === username);
-  if (exists) return res.send('Username exists.');
+  if (exists) {
+    return res.send('Username exists.');
+  }
+
   const newUser = { id: users.length + 1, username, password, role };
   users.push(newUser);
   req.session.user = newUser;
@@ -31,10 +39,14 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
+
   const user = users.find(
     (u) => u.username === username && u.password === password
   );
-  if (!user) return res.send('Invalid credentials');
+  if (!user) {
+    return res.send('Invalid credentials');
+  }
+
   req.session.user = user;
   res.redirect(user.role === 'admin' ? '/admin/dashboard' : '/shop/portal');
 });
