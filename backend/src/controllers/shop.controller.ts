65c794d000b4bdd3251fb38741bce6dac79a4d97
ckphase <worldchampion202428@gmail.com>
2025-04-12 21:13:41 +0530

    fix: typo (wasted '30 min cause of this lol)

--- backend/src/controllers/shop.controller.ts
+++ backend/src/controllers/shop.controller.ts
@@ -2,14 +2,26 @@ import { Request, Response } from 'express';
 import prisma from '../config/db';
 
 export const getAllCategories = async (req: Request, res: Response) => {
-  const catagories = await prisma.category.findMany();
+  const categories = await prisma.category.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
-  res.status(200).json({ catagories });
+  res.status(200).json({ categories });
 };
 
 export const getAllProducts = async (req: Request, res: Response) => {
   const products = await prisma.productVariant.findMany({
     include: {
+      product: {
+        select: {
+          category: {
+            select: { name: true },
+          },
+        },
+      },
       prices: {
         select: {
           shop: {
