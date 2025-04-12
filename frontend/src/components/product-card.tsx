020efff1fda239f617d5998c343350bea2d301e3
ckphase <worldchampion202428@gmail.com>
2025-04-12 22:04:00 +0530

    chore: remove link wrapper in product card

--- frontend/src/components/product-card.tsx
+++ frontend/src/components/product-card.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import { Card, CardContent } from './ui/card';
 
 type ProductCardProps = {
@@ -21,25 +20,23 @@ export const ProductCard = ({
   price,
 }: ProductCardProps) => {
   return (
-    <Link to='/'>
-      <Card className='pt-0'>
-        <img
-          src={image}
-          alt={name}
-          className='object-cover object-top w-full h-48 rounded-t-sm'
-        />
-        <CardContent className='space-y-4'>
-          <div className='space-y-1'>
-            <h3 className='text-2xl font-semibold tracking-tight'>{name}</h3>
-            <p className='text-sm text-muted-foreground'>{description}</p>
-          </div>
-          <div className='flex justify-between'>
-            <p className='text-primary font-bold text-xl'>
-              ₹{price.min} - ₹{price.max}
-            </p>
-          </div>
-        </CardContent>
-      </Card>
-    </Link>
+    <Card className='pt-0'>
+      <img
+        src={image}
+        alt={name}
+        className='object-cover object-top w-full h-48 rounded-t-sm'
+      />
+      <CardContent className='space-y-4'>
+        <div className='space-y-1'>
+          <h3 className='text-2xl font-semibold tracking-tight'>{name}</h3>
+          <p className='text-sm text-muted-foreground'>{description}</p>
+        </div>
+        <div className='flex justify-between'>
+          <p className='text-primary font-bold text-xl'>
+            ₹{price.min} - ₹{price.max}
+          </p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
